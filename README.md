db79aae4b99edccc768c821bd57a931939cd1731
bLeDy <deejaynof@gmail.com>
2020-05-24 19:56:24 +0200

    Using subprocess instead of pyautogui

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Computer Monitoring Automation
+I always have a terminal occupying the lower half of the screen, in my 2nd monitor (vertical),
+with a [tmux] session splitted horizontally.
 
-When i start the computer i always open a new terminal in my 2nd monitor occupying the lower half of the screen.
-Then i will open a new [tmux](https://github.com/tmux/tmux) session and split the
-panel horizontally.
+I always have [htop] in the upper part and [nethogs] in the lower part.
 
 ```bash
 |          |
@@ -17,16 +16,6 @@ panel horizontally.
 |..........|
 ```
 
-I always have [nethogs](https://github.com/raboof/nethogs) in the
-lower part and [htop](https://github.com/hishamhm/htop) in the upper part.
-
-This scripts automates this actions using [PyAutoGUI](https://github.com/asweigart/pyautogui)
-
-1. Open a new terminal
-2. Create tmux session
-3. Move terminal to the 2nd monitor
-4. Resize terminal
-5. Split tmux horizontally
-6. Open nethogs in lower half
-7. Open htop in upper half
-8. Close initial terminal
+[tmux]: https://github.com/tmux/tmux
+[htop]: https://github.com/hishamhm/htop
+[nethogs]: https://github.com/raboof/nethogs
--- watchdog.py
+++ watchdog.py
@@ -1,44 +1,9 @@
 #!/usr/bin/env python3
-import pyautogui
-import time
+import subprocess
+import shlex
 
-# Open new terminal
-pyautogui.hotkey('ctrl', 'alt', 't')
-time.sleep(0.5)
+CONSOLE = shlex.split('alacritty --dimensions 155 64 --position 0 960')
+COMMAND = shlex.split('--command tmux new-session htop ; split-window nethogs -s')
 
-# Create tmux session
-pyautogui.write('tmux')
-pyautogui.press('enter')
-
-# Move terminal to 2nd monitor and resize it
-pyautogui.hotkey('winleft', 'shift', 'left')
-pyautogui.hotkey('winleft', 'right')
-pyautogui.hotkey('winleft', 'down')
-pyautogui.hotkey('winleft', 'down')
-
-# Split tmux horizontally
-pyautogui.hotkey('ctrl', 'a')
-pyautogui.press('-')
-
-# Open nethogs sorting by sent
-pyautogui.write('nethogs -s')
-pyautogui.press('enter')
-
-# Select upper half
-pyautogui.hotkey('alt', 'up')
-
-# Open htop
-pyautogui.write('htop')
-pyautogui.press('enter')
-
-# Leave nethogs selected
-pyautogui.hotkey('alt', 'down')
-
-# Switch to initial terminal
-pyautogui.keyDown('alt')
-pyautogui.press('tab')
-pyautogui.keyUp('alt')
-
-# Quit initial terminal
-pyautogui.write('exit')
-pyautogui.press('enter')
+if __name__ == '__main__':
+    proc = subprocess.Popen([*CONSOLE, *COMMAND])
