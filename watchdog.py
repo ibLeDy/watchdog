0913da33cd91e0fc852b24d652146fbd79944e51
bLeDy <deejaynof@gmail.com>
2020-05-29 02:31:20 +0200

    Don't assing process to an unused variable
    Better variable naming, not constants

--- watchdog.py
+++ watchdog.py
@@ -2,8 +2,8 @@
 import subprocess
 import shlex
 
-CONSOLE = shlex.split('alacritty --dimensions 155 64 --position 0 960')
-COMMAND = shlex.split('--command tmux new-session htop ; split-window nethogs -s')
+terminal = shlex.split('alacritty --dimensions 155 64 --position 0 960')
+programs = shlex.split('--command tmux new-session htop ; split-window nethogs -s')
 
 if __name__ == '__main__':
-    proc = subprocess.Popen([*CONSOLE, *COMMAND])
+    subprocess.Popen([*terminal, *programs])
